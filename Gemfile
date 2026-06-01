0287712eddc73b030403b2b6f24806f33c098f26
James <jchild@i-neda.com>
2026-06-01 15:02:17 +0100

    added bootstrap, adding some spec framework

--- Gemfile
+++ Gemfile
@@ -52,6 +52,10 @@ group :development, :test do
 
   # Omakase Ruby styling [https://github.com/rails/rubocop-rails-omakase/]
   gem "rubocop-rails-omakase", require: false
+
+  gem "rspec-rails"
+
+  gem "factory_bot_rails"
 end
 
 group :development do
@@ -66,3 +70,5 @@ group :test do
 end
 
 gem "dartsass-rails", "~> 0.5.1"
+
+gem "cssbundling-rails", "~> 1.4"
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.1].define(version: 2026_05_11_151631) do
-  create_table "cards", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "cards", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "name"
     t.datetime "updated_at", null: false
   end
 
-  create_table "races", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "races", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "card_id"
     t.datetime "created_at", null: false
     t.string "name"
